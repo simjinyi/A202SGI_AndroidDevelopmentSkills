b335c586b153fadd20ab62ffcfcfd83b446685aa
simjinyi <38304422+simjinyi@users.noreply.github.com>
2019-12-03 00:28:06 +0800

    Settings Unimplemented

---
+++ PointOfSales/app/src/main/java/com/example/pointofsales/viewmodel/StoreSettingsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.pointofsales.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+
+public class StoreSettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
